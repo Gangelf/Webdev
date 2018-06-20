bf6bbdd874086451b237f01d34259b06b92f7c6f
Alex Lyzhoft <alexlyz@gmail.com>
2018-06-19 21:32:03 -0500

    finished React Components

--- React/indecision-app/src/playground/build-it-visible.js
+++ React/indecision-app/src/playground/build-it-visible.js
@@ -1,27 +1,59 @@
-const appRoot = document.getElementById('app');
+class Visible extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            toggled: false
+        }
+    }
+    toggle() {
+        this.setState((prevState) => {
+            return {
+                toggled: !prevState.toggled
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.toggle}>{this.state.toggled ? 'Hide Details' : 'Show Details'}</button>
+                {this.state.toggled && (
+                    <div>
+                        <p>Hey. These are some details you can now see</p>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
 
-let toggled = false;
+ReactDOM.render(<Visible />, document.getElementById('app'));
 
-const toggle = () => {
-    toggled = !toggled
+// const appRoot = document.getElementById('app');
 
-    render();
-};
+// let toggled = false;
 
-const render = () => {
-    const visible = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggle}>{toggled ? 'Hide Details' : 'Show Details'}</button>
-            {toggled && (
-                <div>
-                    <p>Hey. These are some details you can now see</p>
-                </div>
-            )}
-        </div>
-    );
-    ReactDOM.render(visible, appRoot);
-};
+// const toggle = () => {
+//     toggled = !toggled
 
-render();
+//     render();
+// };
+
+// const render = () => {
+//     const visible = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggle}>{toggled ? 'Hide Details' : 'Show Details'}</button>
+//             {toggled && (
+//                 <div>
+//                     <p>Hey. These are some details you can now see</p>
+//                 </div>
+//             )}
+//         </div>
+//     );
+//     ReactDOM.render(visible, appRoot);
+// };
+
+// render();
 
